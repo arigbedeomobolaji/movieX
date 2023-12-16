e4714ef0105a099b17f23c4d8b3277214e9df1d4
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2023-12-16 01:29:21 +0100

    error-page responsiveness

--- client/src/components/error-page.jsx
+++ client/src/components/error-page.jsx
@@ -1,12 +1,24 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import Navbar from "./Navbar";
+import { TiChevronLeft } from "react-icons/ti";
 
 export default function ErrorPage() {
 	const error = useRouteError();
 	console.error(error);
 	return (
-		<div
+		<div className="relative w-full h-full overflow-hidden">
+<Navbar />
+			<div className="absolute top-24 left-10 bottom-7 overflow-hidden">
+			<Link
+				to="/"
+				className="flex gap-2 items-center text-blue-500 font-poppings text-md p-3 pt-5 no-underline"
+			>
+				<TiChevronLeft /> Back to Home
+			</Link>
+			</div>
+			<div
 			id="error-page"
-			className="w-screen h-screen space-y-20 p-5 font-poppings flex flex-col items-center justify-center bg-red-50"
+			className="space-y-18 mt-32  h-full w-full font-poppings flex flex-col items-center justify-center overflow-hidden"
 		>
 			<h1 className="text-[100px] text-red-700">Oops!</h1>
 			<p className="text-[30px] font-bold text-center">
@@ -16,5 +28,7 @@ export default function ErrorPage() {
 				<i>{error.statusText || error.message}</i>
 			</p>
 		</div>
+		</div>
+			
 	);
 }
